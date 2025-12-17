3841acba597d252d98d21389711e02c222307a80
Britah <149816392+Britah@users.noreply.github.com>
2025-12-17 15:12:23 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
-set -o errexit
+#!/bin/bash
+# build.sh
+echo "Python version: $(python --version)"
+echo "Starting build..."
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
+
+echo "Build completed!"
